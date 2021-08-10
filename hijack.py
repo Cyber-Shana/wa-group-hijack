33fad22c33fdb46b71c187722ad76d9891917031
Cyber Shana <87963215+Cyber-Shana@users.noreply.github.com>
2021-08-10 18:07:26 +0530

    Add files via upload

---
+++ hijack.py
@@ -0,0 +1,156 @@
+#Cyber Shana Hijack tool
+import time
+c   = '\33[94m'
+g  = '\33[92m'
+b  = '\33[36m'
+w = '\33[37m'
+y2 = '\33[93m'
+t = '\33[44m'
+bl = '\33[40m'
+f = 1
+y = 2
+z = 3
+a = 4
+time.sleep(0.8)
+print(c +"°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•")
+print()
+time.sleep(0.8)
+print(g +"""
+  __    __   __         __       ___        ______  __  ___  |  |  |  | |  |       |  |     /   \      /      ||  |/  / 
+ |  |__|  | |  |       |  |    /  ^  \    |  ,----'|  '  /  
+ |   __   | |  | .--.  |  |   /  /_\  \   |  |     |    <   
+ |  |  |  | |  | |  `--'  |  /  _____  \  |  `----.|  .  \  
+ |__|  |__| |__|  \______/  /__/     \__\  \______||__|\__\ 
+                                                            
+ ____    __    ____   ______ 
+ \   \  /  \  /   /  /      |
+  \   \/    \/   /  |  ,----'
+   \            /   |  |     
+    \    /\    /    |  `----.
+     \__/  \__/      \______|
+                             
+   _______ .______        ______    __    __  .______   
+  /  _____||   _  \      /  __  \  |  |  |  | |   _  \  
+ |  |  __  |  |_)  |    |  |  |  | |  |  |  | |  |_)  | 
+ |  | |_ | |      /     |  |  |  | |  |  |  | |   ___/  
+ |  |__| | |  |\  \----.|  `--'  | |  `--'  | |  |      
+  \______| | _| `._____| \______/   \______/  |    """)
+print('\n')
+print()
+time.sleep(0.8)
+print(y2 + "°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•°•")
+print()
+time.sleep(0.8)
+print(w +"                   ✨️°Tool Ctrate By°✨️                 ")
+print()
+time.sleep(0.8)
+print(b +"                     '🎭Cyber SHANA👑                      ")
+print()
+time.sleep(0.8)
+print("\033[31m                ⚖️ᴼʷⁿᵉʳ_¢увєя вα¢к ѕнσт 🎭                        ")
+print()
+time.sleep(0.8)
+print("\033[95m *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*")
+print()
+print()
+time.sleep(0.8)
+x = str(input(y2 +"Enter Group link:-"))
+print()
+time.sleep(0.8)
+y = str(input(y2 +"Enter Group creater number:-"))
+print()
+time.sleep(0.8)
+print(w+"wait........")
+time.sleep(0.8)
+print("wait........")
+time.sleep(0.8)
+print("wait........")
+time.sleep(0.8)
+print("wait........")
+time.sleep(0.8)
+print("wait........")
+time.sleep(0.8)
+print("wait........")
+time.sleep(0.8)
+print("wait........")
+time.sleep(0.8)
+print("wait........")
+time.sleep(0.8)
+print("wait........")
+time.sleep(0.8)
+print("wait........")
+time.sleep(0.8)
+print("wait........")
+time.sleep(0.8)
+print("wait........")
+time.sleep(0.8)
+print("wait........")
+print("wait........")
+time.sleep(0.8)
+print("wait........")
+print("wait........")
+print("wait........")
+time.sleep(0.8)
+print("wait........")
+print("wait........")
+time.sleep(0.8)
+print("wait........")
+print()
+time.sleep(0.8)
+Group = str(input(y2 +"Enter Your Number:-"))
+print("")
+print()
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print(b +"The group is being hijacked")
+print(b +"The group is being hijacked")
+print(b +"The group is being hijacked")
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print(b +"The group is being hijacked")
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print(b +"The group is being hijacked")
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print(b +"The group is being hijacked")
+print(b +"The group is being hijacked")
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print(b +"The group is being hijacked")
+time.sleep(0.8)
+print()
+print()
+time.sleep(0.8)
+x = str(input(y2 +"Please provide (Y / N)"))
+print()
+time.sleep(0.8)
+print("\033[1;33;40m 🤣💨💦💥✋උඹ හිතුවද එච්චර ලේසියෙන් Group hijack ක කරන්න😂💔\n \033[0m " *9999)
